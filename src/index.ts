856df91e33bb27d0af7c668aa1b69e2883ec4c9e
Michal Panský <pansky.michal@gmail.com>
2022-04-26 16:15:05 +0200

    init package and add types

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+export * from './types/Availability';
+export * from './types/AvailabilityCalendar';
+
+export * from './types/Booking';
+
+export * from './types/Capability';
+
+export * from './types/Currency';
+
+export * from './types/Duration';
+
+export * from './types/Option';
+
+export * from './types/PickupPoint';
+
+export * from './types/Pricing';
+
+export * from './types/Product';
+
+export * from './types/Supplier';
+
+export * from './types/Unit';
