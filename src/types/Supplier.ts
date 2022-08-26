05c400dc7d01e4453703d65becd516e354fa5293
Michal Panský <pansky.michal@gmail.com>
2022-08-26 12:28:28 +0200

    add supplier content

--- src/types/Supplier.ts
+++ src/types/Supplier.ts
@@ -1,4 +1,4 @@
-export interface Supplier {
+export interface Supplier extends SupplierContent {
   id: string;
   name: string;
   endpoint: string;
@@ -10,4 +10,30 @@ interface SupplierContact {
   email: Nullable<string>;
   telephone: Nullable<string>;
   address: Nullable<string>;
+}
+
+interface SupplierContent {
+  country: string;
+  destinations: Array<Destination>;
+}
+
+interface Destination {
+  id: string;
+  default: boolean;
+  name: string;
+  country: string;
+  contact: SupplierContact;
+  latitude: number;
+  longitude: number;
+  categories: Array<Category>;
+}
+
+interface Category {
+  id: string;
+  default: boolean;
+  title: string;
+  shortDescription: Nullable<string>;
+  coverImageUrl: Nullable<string>;
+  bannerImageUrl: Nullable<string>;
+  productIds: Array<string>;
 }
