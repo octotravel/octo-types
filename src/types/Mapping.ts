38ba4f2022723c4d400c77e1448a48f7b75ba0d0
Michal Panský <pansky.michal@gmail.com>
2022-09-22 16:49:06 +0200

    add mapping type

---
+++ src/types/Mapping.ts
@@ -0,0 +1,13 @@
+export interface OctoMapping {
+    resellerReference: string;
+    title: string;
+    url: string;
+    webhookUrl: Nullable<string>;
+    optionRequired: boolean;
+    unitRequired: boolean;
+    productId: Nullable<string>;
+    optionId: Nullable<string>;
+    unitId: Nullable<string>;
+    connected: boolean;
+}
+  
